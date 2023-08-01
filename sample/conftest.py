b8470d130d316b043b4b1043e1f9e838c1d0c27d
Jan Zeleny <zelenja8@gmail.com>
2023-08-01 13:02:34 +0200

    Initial commit

---
+++ sample/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from sample.users.models import User
+from sample.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
